0ccb5a0709ee0dbfea106f6a70b5bccac4f0f3e3
sarvaiya-pratik <pratikrajput125@gmail.com>
2023-06-01 19:57:03 +0530

    second

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,28 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 
 const Home = () => {
   return (
     <Box bgColor='blackAlpha.900' w='full' h="85vh">
-      <Image  w="full" h="full" objectFit='contain' src={"https://freepngimg.com/thumb/bitcoin/59621-cryptocurrency-coinbase-litecoin-blockchain-bitcoin-free-download-png-hd-thumb.png"} />
+      <motion.div 
+      style={{height:"80vh",padding:"30px"}}
+      animate={{
+        translateY:"20px"
+      }}
 
-      <Text color='white' marginTop="-20"  fontSize={'6xl'} textAlign='center' fontWeight='thin'>Crypto</Text>
+      transition={{
+        duration:2,repeat:Infinity,repeatType:"reverse",
+      }}
+      
+      >
+
+      <Image  w="full" h="full" objectFit='contain' src={"https://static.currency.com/img/media/bitcoin.dd8a16.png"} />
+      </motion.div>
+
+
+      <Text color='white' marginTop="-10"  fontSize={'6xl'} textAlign='center' fontWeight='thin'>Crypto</Text>
 
     </Box>
   )
